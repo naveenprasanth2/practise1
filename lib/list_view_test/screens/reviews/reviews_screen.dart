c71cebfbf8ee248d7edcf8d3687a0d85a0a89fe9
naveenprasanth2 <78147993+naveenprasanth2@users.noreply.github.com>
2023-07-17 10:50:38 +0530

    tillRatingsImplementation

--- lib/list_view_test/screens/reviews/reviews_screen.dart
+++ lib/list_view_test/screens/reviews/reviews_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:practise1/list_view_test/widgets/ratings/ratings_widget.dart';
 
 class ReviewsScreen extends StatefulWidget {
-  const ReviewsScreen({super.key});
+  const ReviewsScreen({Key? key}) : super(key: key);
 
   @override
   State<ReviewsScreen> createState() => _ReviewsScreenState();
@@ -16,9 +16,8 @@ class _ReviewsScreenState extends State<ReviewsScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
-    getDetailedRatingsFromJson();
     super.initState();
+    getDetailedRatingsFromJson();
   }
 
   Future<void> getDetailedRatingsFromJson() async {
@@ -36,77 +35,100 @@ class _ReviewsScreenState extends State<ReviewsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red.shade400,
-        title: const Text(
-          "Ratings & Reviews",
-          style: (TextStyle(
-            fontWeight: FontWeight.bold,
-          )),
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.red.shade100),
-                    ),
-                    child: const Align(
-                      alignment: Alignment.center,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(Icons.star),
-                              SizedBox(width: 6),
-                              Text("4.5"),
-                            ],
-                          ),
-                          Text("out of 5"),
-                        ],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.red.shade400,
+            title: const Text(
+              "Ratings & Reviews",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            centerTitle: true,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.red.shade100),
+                      ),
+                      child: const Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.star),
+                                SizedBox(width: 6),
+                                Text("4.5"),
+                              ],
+                            ),
+                            Text("out of 5"),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                const SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Good"),
-                      Text("53 Ratings"),
-                    ],
+                  const SizedBox(
+                    width: 20,
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good"),
+                        Text("53 Ratings"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            const RatingStatsWidget(
-                count5Stars: 1,
-                count4Stars: 4,
-                count3Stars: 9,
-                count2Stars: 7,
-                count1Star: 5),
-          ],
-        ),
+          ),
+          const SliverToBoxAdapter(
+            child: RatingStatsWidget(
+              count5Stars: 1,
+              count4Stars: 4,
+              count3Stars: 9,
+              count2Stars: 7,
+              count1Star: 5,
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 20,),),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Container(
+                    height: 200,
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                    decoration: BoxDecoration(color: Colors.white, border: Border.all(
+                      color: Colors.black26
+                    )),
+                  ),
+                );
+              },
+              childCount: ratingsDetails.length,
+            ),
+          ),
+        ],
       ),
     );
   }
