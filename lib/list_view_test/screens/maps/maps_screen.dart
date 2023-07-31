0b7373f1ad0c1edd960e5fbe035d37e1aadabc44
naveenprasanth2 <78147993+naveenprasanth2@users.noreply.github.com>
2023-07-31 15:38:45 +0530

    refactoredMapsProvider

--- lib/list_view_test/screens/maps/maps_screen.dart
+++ lib/list_view_test/screens/maps/maps_screen.dart
@@ -1,10 +1,8 @@
 import 'dart:convert';
-import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:map_launcher/map_launcher.dart' as map_launcher;
-import 'package:practise1/list_view_test/models/nearby_places_model/place_category_model.dart';
 import 'package:practise1/list_view_test/providers/maps_provider.dart';
 import 'package:provider/provider.dart';
 import '../../models/nearby_places_model/nearby_places_model.dart';
@@ -18,7 +16,6 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  final Map<String, Marker> _markers = {};
   NearbyPlacesModel? nearbyPlaces;
   GoogleMapController? mapController;
 
@@ -37,135 +34,22 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
-  Future<BitmapDescriptor> _createCustomMarkerIcon(String path) async {
-    final byteData = await rootBundle.load(path);
-    var codec = await ui.instantiateImageCodec(byteData.buffer.asUint8List(),
-        targetWidth: 120);
-    var frame = await codec.getNextFrame();
-    final data = await frame.image.toByteData(format: ui.ImageByteFormat.png);
-    return BitmapDescriptor.fromBytes(data!.buffer.asUint8List());
-  }
-
-  Future<BitmapDescriptor> _createCustomMarkerIconForMainPlace(
-      String path) async {
-    final byteData = await rootBundle.load(path);
-    var codec = await ui.instantiateImageCodec(byteData.buffer.asUint8List(),
-        targetWidth: 170);
-    var frame = await codec.getNextFrame();
-    final data = await frame.image.toByteData(format: ui.ImageByteFormat.png);
-    return BitmapDescriptor.fromBytes(data!.buffer.asUint8List());
-  }
-
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
     Provider.of<MapProvider>(context, listen: false)
         .setLatAndLng(nearbyPlaces!);
-    _addMarkers();
-  }
-
-  void _addMarkers() async {
-    if (nearbyPlaces == null || mapController == null) return;
-
-    final hotelIcon =
-        await _createCustomMarkerIconForMainPlace('assets/hotel_icon.png');
-    final othersIcon = await _createCustomMarkerIcon('assets/others_icon.png');
-    final transportIcon =
-        await _createCustomMarkerIcon('assets/transport_icon.png');
-    final mallsRestaurantsIcon =
-        await _createCustomMarkerIcon('assets/malls_restaurants_icon.png');
-    final popularPlacesIcon =
-        await _createCustomMarkerIcon('assets/popular_places_icon.png');
-
-    final hotelMarker = Marker(
-      markerId: MarkerId(nearbyPlaces!.hotelLocationDetails.name),
-      onTap: () {
-        Provider.of<MapProvider>(context, listen: false).setClickedLatAndLng(
-            nearbyPlaces!.hotelLocationDetails.lat,
-            nearbyPlaces!.hotelLocationDetails.lng,
-            nearbyPlaces!.hotelLocationDetails.name);
-      },
-      icon: hotelIcon,
-      position: LatLng(
-        nearbyPlaces!.hotelLocationDetails.lat,
-        nearbyPlaces!.hotelLocationDetails.lng,
-      ),
-      infoWindow: InfoWindow(
-        title: nearbyPlaces!.hotelLocationDetails.name,
-        snippet: nearbyPlaces!.hotelLocationDetails.address,
-      ),
-    );
-    _markers['hotel'] = hotelMarker;
-
-    _addCategoryMarkers(nearbyPlaces!.transport, transportIcon);
-    _addCategoryMarkers(
-        nearbyPlaces!.mallsAndRestaurants, mallsRestaurantsIcon);
-    _addCategoryMarkers(nearbyPlaces!.popularPlaces, popularPlacesIcon);
-    _addCategoryMarkers(nearbyPlaces!.others, othersIcon);
-
-    setState(() {}); // To trigger marker addition on the map
-  }
-
-  void _addCategoryMarkers(
-      List<PlaceCategoryModel> places, BitmapDescriptor icon) {
-    for (final place in places) {
-      final marker = Marker(
-        markerId: MarkerId(place.name),
-        onTap: () {
-          Provider.of<MapProvider>(context, listen: false)
-              .setClickedLatAndLng(place.lat, place.lng, place.name);
-        },
-        position: LatLng(place.lat, place.lng),
-        icon: icon,
-        infoWindow: InfoWindow(
-          title: place.name,
-          snippet: place.address,
-        ),
-      );
-      _markers[place.name] = marker;
-    }
-  }
-
-  void _openMap() async {
-    await map_launcher.MapLauncher.installedMaps.then(
-      (availableMaps) => {
-        if (availableMaps.isNotEmpty)
-          {
-            availableMaps[0].showMarker(
-              coords: map_launcher.Coords(
-                  Provider.of<MapProvider>(context, listen: false).clickedLat,
-                  Provider.of<MapProvider>(context, listen: false).clickedLng),
-              title: Provider.of<MapProvider>(context, listen: false)
-                  .clickedPlaceName,
-            ),
-          }
-        else
-          {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('No Maps Apps Installed'),
-                  content: const Text('Please install a maps app to navigate.'),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('OK'),
-                    ),
-                  ],
-                );
-              },
-            ),
-          },
-      },
-    );
+    Provider.of<MapProvider>(context, listen: false)
+        .addMarkers(nearbyPlaces!, mapController!, context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Consumer<MapProvider>(builder: (context, provider, _) {
+      body: Consumer<MapProvider>(builder: (
+        context,
+        mapProvider,
+        _,
+      ) {
         return Stack(
           children: [
             (nearbyPlaces == null)
@@ -181,7 +65,7 @@ class _MapScreenState extends State<MapScreen> {
                       ),
                       zoom: 16,
                     ),
-                    markers: _markers.values.toSet(),
+                    markers: mapProvider.markers.values.toSet(),
                   ),
             Positioned(
               top: 50,
@@ -212,7 +96,7 @@ class _MapScreenState extends State<MapScreen> {
                 },
                 child: FloatingActionButton(
                   onPressed: () {
-                    _openMap();
+                    mapProvider.openMap(context);
                   },
                   backgroundColor: Colors.red.shade400,
                   child: const Icon(
