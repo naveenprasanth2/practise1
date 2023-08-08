64e1b6f5a47fd7a03d9588e09044dbe91e8bba67
naveenprasanth2 <78147993+naveenprasanth2@users.noreply.github.com>
2023-08-08 20:56:21 +0530

    firebaseIntegrationDone

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:practise1/list_view_test/flash_screen/flash_screen.dart';
 import 'package:practise1/list_view_test/providers/calculation_provider.dart';
@@ -8,7 +9,9 @@ import 'package:provider/provider.dart';
 
 import 'list_view_test/providers/coupon_state_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
