38920f9fcb53906b21c88cb723e1a18a4a0094bb
naveenprasanth2 <78147993+naveenprasanth2@users.noreply.github.com>
2023-09-01 13:42:05 +0530

    functionUploaded

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,51 +1,85 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as crypto from 'crypto';
+import Razorpay from 'razorpay';
 
 admin.initializeApp();
 
-export const handleRazorpayCallback = functions.https.onRequest((req, res) => {
-  const secret = 'TL2uYZPe5DoDOcaaP4Zr8ua1';
-  const razorpaySignature = req.headers['x-razorpay-signature'] as string;
-  const body = JSON.stringify(req.body);
+const db = admin.firestore();
 
-  const generatedSignature = crypto.createHmac('sha256', secret).update(body).digest('hex');
-
-  if (generatedSignature === razorpaySignature) {
-    // signature is valid
-    const event = req.body.event;
-
-    if (event === 'payment.authorized') {
-      // Extract email and contact from payload
-      const mobileNo = req.body.payload.payment.entity.contact;
-      const description = req.body.payload.payment.entity.description;
-      const notesString = req.body.payload.payment.entity.notes;
-        // Parse the valid notesString JSON
-        let notes = {
-            ...JSON.parse(notesString.paymentDetails),
-            ...JSON.parse(notesString.hotelDetails),
-            ...JSON.parse(notesString.bookingSchedule),
-            ...JSON.parse(notesString.bookingStatus),
-        };
-      // Update Firestore database
-      const userDocRef = admin.firestore().collection('users').doc(mobileNo); // Assuming mobile number is used as the user's ID
-      const bookingDocRef = userDocRef.collection('bookings').doc(description);
-      notes.status = "success";
-
-      bookingDocRef.set({ description: notes })
-        .then(() => {
-          res.status(200).send('OK');
-        })
-        .catch(error => {
-          console.error('Error updating database:', error);
-          res.status(500).send('Internal server error');
-        });
-    } else {
-      // ignore other events
-      res.status(200).send('OK');
-    }
-  } else {
-    // signature is not valid
-    res.status(400).send('Signature not valid');
-  }
+const razorpay = new Razorpay({
+    key_id: 'rzp_test_w1r1PYTH0wy118',
+    key_secret: 'TL2uYZPe5DoDOcaaP4Zr8ua1'
 });
+
+const processRefund = functions.https.onRequest(async (request, response) => {
+    const userId = request.body.userId;
+    const orderId = request.body.orderId;
+    const amount = request.body.amount;
+
+    try {
+        const booking = await db.doc(`users/${userId}/bookings/${orderId}`).get()
+            .then(doc => doc.data());
+
+        if (!booking) {
+            response.status(404).json({ error: 'Booking not found' });
+            return;
+        }
+
+        if (booking.status !== 'success') {
+            response.status(400).json({ error: 'We cannot process refund for this orderId' });
+            return;
+        }
+
+        const paymentId = booking.paymentId;
+
+        const refund = await razorpay.payments.refund(paymentId, { amount });
+        response.json(refund);
+    } catch (error) {
+        response.status(500).json(error);
+    }
+});
+
+const handleRazorpayCallback = functions.https.onRequest((req, res) => {
+    const secret = 'TL2uYZPe5DoDOcaaP4Zr8ua1';
+    const razorpaySignature = req.headers['x-razorpay-signature'] as string;
+    const body = JSON.stringify(req.body);
+
+    const generatedSignature = crypto.createHmac('sha256', secret).update(body).digest('hex');
+
+    if (generatedSignature === razorpaySignature) {
+        const event = req.body.event;
+
+        if (event === 'payment.authorized') {
+            const mobileNo = req.body.payload.payment.entity.contact;
+            const description = req.body.payload.payment.entity.description;
+            const notesString = req.body.payload.payment.entity.notes;
+
+            let notes = {
+                ...JSON.parse(notesString.paymentDetails),
+                ...JSON.parse(notesString.hotelDetails),
+                ...JSON.parse(notesString.bookingSchedule),
+                ...JSON.parse(notesString.bookingStatus),
+            };
+
+            const userDocRef = admin.firestore().collection('users').doc(mobileNo);
+            const bookingDocRef = userDocRef.collection('bookings').doc(description);
+            notes.status = "success";
+
+            bookingDocRef.set({ description: notes })
+                .then(() => {
+                    res.status(200).send('OK');
+                })
+                .catch(error => {
+                    console.error('Error updating database:', error);
+                    res.status(500).send('Internal server error');
+                });
+        } else {
+            res.status(200).send('OK');
+        }
+    } else {
+        res.status(400).send('Signature not valid');
+    }
+});
+
+export { processRefund, handleRazorpayCallback };
